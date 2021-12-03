16371659f660bf7dd5553117eb08c3dc3eede886
pesch33 <peter@moonmanager.de>
2021-12-03 19:42:47 +0100

    2 files modified

--- src/app/hello-world/hello-world.component.ts
+++ src/app/hello-world/hello-world.component.ts
@@ -1,17 +1,15 @@
-
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'hello-world',
   template: `
-    <h2>Hello World</h2>
+    <h2>Hello World22</h2>
     <p>This is my first component!</p>
-  `
+  `,
 })
 export class HelloWorldComponent {
   // The code in this class drives the component's behavior.
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
